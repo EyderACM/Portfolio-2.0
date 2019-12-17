c6b7e9220fd778cd2248aed208fc733c22e143da
EyderACM <eyderacm@gmail.com>
2019-12-16 22:21:39 -0600

    Add design utils

---
+++ utils/pixelToRem.js
@@ -0,0 +1,3 @@
+export const pixelToRem = (pixels) => {
+    return `${pixels/16}rem`;
+}
---
+++ utils/units.js
@@ -0,0 +1,7 @@
+// Desktop Width
+export const MIN_WIDTH_DESKTOP = '1025px';
+export const MAX_WIDTH_DESKTOP = '1080px';
+
+// Tablet Witdh
+export const MIN_TABLET_WIDTH = '768px';
+export const TABLET_TABLET_WIDTH = '1024px';
