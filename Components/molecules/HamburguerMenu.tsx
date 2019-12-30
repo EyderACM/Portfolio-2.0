04c12aa26df2705f6fca839caca8e95ae778d86b
EyderACM <eyderacm@gmail.com>
2019-12-29 22:50:23 -0600

    Impl: implement animaton for nav items

--- Components/molecules/HamburguerMenu.tsx
+++ Components/molecules/HamburguerMenu.tsx
@@ -3,7 +3,7 @@ import { HamburguerMenuContainer } from '../atoms/HamburguerMenuContainer';
 import { HamburguerMenuWrapper } from '../atoms/HamburguerMenuWrapper';
 import { HamburguerMenuText } from '../atoms/HamburguerMenuText';
 // @ts-ignore
-import { TweenMax, gsap, Circ, Back } from 'gsap/dist/gsap';
+import { TweenMax, gsap, Circ, TimelineMax, Power3 } from 'gsap/dist/gsap';
 import { pixelToRem } from '../../utils/pixelToRem';
 // @ts-ignore
 
@@ -16,6 +16,10 @@ export interface IHamburguerMenu {
 // TODO add type
 export const HamburguerMenu = ({ state }: any) => {
   let hamburguerMenu: any = useRef(null);
+  let text1: any = useRef(null);
+  let text2: any = useRef(null);
+  let text3: any = useRef(null);
+  let text4: any = useRef(null);
 
   useEffect(() => {
     const hamburguerPosition = `${pixelToRem(window.innerWidth - 55)}`;
@@ -23,19 +27,25 @@ export const HamburguerMenu = ({ state }: any) => {
       TweenMax.to(hamburguerMenu, 1, { css: { clipPath: `circle(${pixelToRem(19)} at ${hamburguerPosition} ${pixelToRem(55)})` }, ease: Circ.easeInOut });
       gsap.to(hamburguerMenu, { duration: 1, css: { display: "none" } });
     }
-    else if (state.isActive === true || (state.isActive === true && state.initial === null)) {
-      gsap.to(hamburguerMenu, { duration: 0, css: { display: "block" } });
-      TweenMax.fromTo(hamburguerMenu, 1, { css: { clipPath: `circle(${pixelToRem(2)} at ${hamburguerPosition} ${pixelToRem(55)})` }} ,{ css: { clipPath: `circle(${pixelToRem(1100)} at ${hamburguerPosition} ${pixelToRem(55)})` }, ease: Circ.easeInOut });
+    else if (state.isActive === true || (state.isActive === true && state.initial === null)) {      
+      gsap.to(hamburguerMenu, { duration: 0, css: { display: "block" } });      
+      fadeInText({text1, text2, text3, text4, hamburguerPosition});
     }
   });
+
+  const fadeInText = ({text1, text2, text3, text4, hamburguerPosition}: any) => {
+    const openingTl = new TimelineMax();
+    openingTl.fromTo(hamburguerMenu, 1, { css: { clipPath: `circle(${pixelToRem(2)} at ${hamburguerPosition} ${pixelToRem(55)})` }} ,{ css: { clipPath: `circle(${pixelToRem(1100)} at ${hamburguerPosition} ${pixelToRem(55)})` }, ease: Circ.easeInOut });    
+    openingTl.from([text4, text3, text2, text1], {duration: 0.085, y: 30, opacity: 0, skewY: 2, ease: Circ.easeInOut, stagger: {amount: 0.3}}, "-=.4");
+  }
 
   return (
     <HamburguerMenuContainer ref={elem => { hamburguerMenu = elem }}>
       <HamburguerMenuWrapper>
-        <HamburguerMenuText>Home</HamburguerMenuText>
-        <HamburguerMenuText>About Me</HamburguerMenuText>
-        <HamburguerMenuText>Skills</HamburguerMenuText>
-        <HamburguerMenuText>Projects</HamburguerMenuText>
+        <HamburguerMenuText ref={elem => { text1 = elem }}>Home</HamburguerMenuText>
+        <HamburguerMenuText ref={elem => { text2 = elem }}>About Me</HamburguerMenuText>
+        <HamburguerMenuText ref={elem => { text3 = elem }}>Skills</HamburguerMenuText>
+        <HamburguerMenuText ref={elem => { text4 = elem }}>Projects</HamburguerMenuText>
       </HamburguerMenuWrapper>
     </HamburguerMenuContainer>
   )
