d0a0ff68bf456f785fa21b2a5e282e57a22076f7
EyderACM <eyderacm@gmail.com>
2019-12-29 18:13:36 -0600

    Fix: hamburger menu animation

--- Components/atoms/HamburguerMenuContainer.tsx
+++ Components/atoms/HamburguerMenuContainer.tsx
@@ -4,7 +4,6 @@ import { pixelToRem } from '../../utils/pixelToRem';
 export const HamburguerMenuContainer = styled.div`
   bottom: 0;
   background: #ff6800;
-  clip-path: circle(${pixelToRem(15)} at 95vw 10vh);
   display: none;
   height: 100%;
   left: 0;
@@ -13,6 +12,5 @@ export const HamburguerMenuContainer = styled.div`
   top: 0;
   right: 0;
   width: 100%;
-  -webkit-clip-path: circle(${pixelToRem(15)} at 84.3% 9%);
   z-index: 1;
 `;
--- Components/molecules/HamburguerMenu.tsx
+++ Components/molecules/HamburguerMenu.tsx
@@ -3,7 +3,9 @@ import { HamburguerMenuContainer } from '../atoms/HamburguerMenuContainer';
 import { HamburguerMenuWrapper } from '../atoms/HamburguerMenuWrapper';
 import { HamburguerMenuText } from '../atoms/HamburguerMenuText';
 // @ts-ignore
-import { TweenMax, gsap, Circ } from 'gsap/dist/gsap';
+import { TweenMax, gsap, Circ, Back } from 'gsap/dist/gsap';
+import { pixelToRem } from '../../utils/pixelToRem';
+// @ts-ignore
 
 export interface IHamburguerMenu {
   menuName?: string;
@@ -12,23 +14,23 @@ export interface IHamburguerMenu {
 }
 
 // TODO add type
-export const HamburguerMenu = ({state}: any) => {
-  let hamburguerMenu: any = useRef(null)
+export const HamburguerMenu = ({ state }: any) => {
+  let hamburguerMenu: any = useRef(null);
 
-  useEffect(() => {    
-    console.log(state.isActive);
-    if(state.isActive === false){
-      TweenMax.to(hamburguerMenu, .7, {clipPath: `circle(16px at 84.3% 9%)`, ease: Circ.easeInOut});      
-      gsap.to(hamburguerMenu, {duration: .7, css: {display: "none"}});
+  useEffect(() => {
+    const hamburguerPosition = `${pixelToRem(window.innerWidth - 55)}`;
+    if (state.isActive === false) {
+      TweenMax.to(hamburguerMenu, .7, { css: { clipPath: `circle(${pixelToRem(19)} at ${hamburguerPosition} ${pixelToRem(55)})` }, ease: Circ.easeInOut });
+      gsap.to(hamburguerMenu, { duration: .7, css: { display: "none" } });
     }
-    else if(state.isActive === true || (state.isActive === true && state.initial === null)){
-      gsap.to(hamburguerMenu, {duration: 0, css: {display: "block"}});
-      TweenMax.to(hamburguerMenu, 1, {clipPath: `circle(2000px at 84.3% 10%)`, ease: Circ.easeInOut});      
+    else if (state.isActive === true || (state.isActive === true && state.initial === null)) {
+      gsap.to(hamburguerMenu, { duration: 0, css: { display: "block" } });
+      TweenMax.fromTo(hamburguerMenu, 1, { css: { clipPath: `circle(${pixelToRem(2)} at ${hamburguerPosition} ${pixelToRem(55)})` }} ,{ css: { clipPath: `circle(${pixelToRem(1100)} at ${hamburguerPosition} ${pixelToRem(55)})` }, ease: Circ.easeInOut });
     }
   });
 
   return (
-    <HamburguerMenuContainer ref={elem => {hamburguerMenu = elem}}>
+    <HamburguerMenuContainer ref={elem => { hamburguerMenu = elem }}>
       <HamburguerMenuWrapper>
         <HamburguerMenuText>Home</HamburguerMenuText>
         <HamburguerMenuText>About Me</HamburguerMenuText>
